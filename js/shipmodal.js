d8175679b14c96ca906a3791f980532e03fb23b0
Helmi74130 <88458882+Helmi74130@users.noreply.github.com>
2022-11-27 14:08:15 +0100

    add modal

--- js/shipmodal.js
+++ js/shipmodal.js
@@ -1,6 +1,34 @@
 let shipment = document.querySelector('.shipment')
 let modalShipment = document.querySelector('.modal-shipment')
+let buttonCloseModalshipment = document.querySelector('#buttonCloseModalshipment')
+let shoesDescriptionContent = document.querySelector('.shoes-description-content')
 
 shipment.addEventListener('click', ()=>{
-  console.log('piuo');
+  openModal()
 })
+
+buttonCloseModalshipment.addEventListener('click', ()=>{
+  closeModal()
+})
+
+function closeModal(){
+  modalShipment.classList.add("swing-out-top-bck");
+    setTimeout(() => {
+      modalShipment.classList.add('none')
+      shoesDescriptionContent.classList.remove("none");
+      modalShipment.classList.remove("swing-out-top-bck");
+      modalShipment.classList.remove("swing-in-top-fwd");
+      shoesDescriptionContent.classList.add("swing-in-top-fwd");
+    }, "700")
+}
+
+function openModal(){
+  shoesDescriptionContent.classList.add("swing-out-top-bck");
+    setTimeout(() => {
+      modalShipment.classList.remove("none");
+      shoesDescriptionContent.classList.add('none')
+      modalShipment.classList.add("swing-in-top-fwd");
+      shoesDescriptionContent.classList.remove("swing-out-top-bck");
+      shoesDescriptionContent.classList.remove("swing-in-top-fwd");
+    }, "700")
+}
