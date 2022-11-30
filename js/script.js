052243b2fda67640dfa5b7a96bb630fdcca36d51
Helmi74130 <88458882+Helmi74130@users.noreply.github.com>
2022-11-30 08:23:29 +0100

    add responsive main

--- js/script.js
+++ js/script.js
@@ -39,65 +39,42 @@ function shoesParallax(e) {
   const speed = shoes.getAttribute('data-speed')
 
   const x = (window.innerWidth - e.pageX*speed)/100
-  const y = (window.innerHeight - e.pageY*speed)
+  const y = (window.innerHeight - e.pageY*speed)*2
 
   shoes.style.transform = `rotateX(${x}deg)`
   shoes.style.transform = `rotateY(${x}deg)`
   bgShoes.style.transform = `rotate(${y}deg)`
 }
 
-window.addEventListener('scroll', ()=>{
-  //console.log(images.getBoundingClientRect().y);
-  if (images.getBoundingClientRect().y < 0 && images.getBoundingClientRect().y < 100) {
-    descriptionContent.classList.add('fixed')
-  }else if (images.getBoundingClientRect().y > 0 && images.getBoundingClientRect().y > 100){
-    descriptionContent.classList.remove('fixed')
-  }
-})
 
 
+if (window.matchMedia("(min-width: 542px)").matches) {
+  window.addEventListener('scroll', ()=>{
+    if (images.getBoundingClientRect().y < 0 && images.getBoundingClientRect().y < 100) {
+      descriptionContent.classList.add('fixed')
+    }else if (images.getBoundingClientRect().y > 0 && images.getBoundingClientRect().y > 100){
+      descriptionContent.classList.remove('fixed')
+    }
+  })
+  
+  
+  const observerLastImg = new IntersectionObserver((entries)=>{
+    if(entries[0].isIntersecting ){
+      descriptionContent.classList.remove('top')
+      descriptionContent.classList.remove('fixed')
+      descriptionContent.classList.add('absolute')
+    }
+  }, {threshold: 0.99});
+  
+  observerLastImg.observe(lastImg)
+  
+  
+  window.addEventListener('scroll', ()=>{
+    if (descriptionContent.getBoundingClientRect().y > 200 && descriptionContent.classList.contains('absolute')) {
+      descriptionContent.classList.add('fixed')
+      descriptionContent.classList.remove('absolute')
+      descriptionContent.classList.add('top')
+    }
+  })
+}
 
-const observerLastImg = new IntersectionObserver((entries)=>{
-  if(entries[0].isIntersecting ){
-    descriptionContent.classList.remove('top')
-    descriptionContent.classList.remove('fixed')
-    descriptionContent.classList.add('absolute')
-    console.log('vu');
-  }
-}, {threshold: 0.99});
-
-observerLastImg.observe(lastImg)
-
-
-
-window.addEventListener('scroll', ()=>{
-  //console.log(descriptionContent.getBoundingClientRect().y);
-  if (descriptionContent.getBoundingClientRect().y > 200 && descriptionContent.classList.contains('absolute')) {
-    descriptionContent.classList.add('fixed')
-    descriptionContent.classList.remove('absolute')
-    descriptionContent.classList.add('top')
-  }
-})
-
-
-
-
-/* const observerDescr = new IntersectionObserver((entries)=>{
-  if(entries[0].isIntersecting){
-    descriptionContent.classList.add('fixed')
-  }
-}, {threshold: 0.33});
-
-const observerImage = new IntersectionObserver((entries)=>{
-  if(!entries[0].isIntersecting){
-    descriptionContent.classList.remove('fixed')
-    //descriptionContent.classList.add('absolute')
-  }
-}, {threshold: 0.33});
-
-
-
-observerImage.observe(images)
-
-observerDescr.observe(descriptionContent)
- */
